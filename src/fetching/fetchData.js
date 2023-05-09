769e081022df5735ca799bbffe6d8b41fe553345
Vincent Kho <vincentkho67@gmail.com>
2023-05-10 00:59:13 +0700

    fetch products on dashboard

--- src/fetching/fetchData.js
+++ src/fetching/fetchData.js
@@ -1,11 +1,18 @@
 export const baseUrl = 'http://localhost:3001/'
 
+const fetchData = async (url) => {
+    const response = await fetch(`${baseUrl}${url}`, {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${sessionStorage.getItem('accessToken')}`,
+        }
+    });
+    const data = await response.json();
+    return data;
+}
 
-// export async function fetchUserData() {
-//     const res = await fetch(`${baseUrl}/users`);
-//     const data = await res.json();
-//     return data;
-
-// };
-
+export const fetchProducts = async () => {
+    const data = await fetchData('products');
+    return data.products;
+};
   
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,9 +1,25 @@
+import {useState, useEffect} from 'react';
+import { fetchProducts } from '@/fetching/fetchData';
+
 const Dashboard = () => {
+  const [data, setData] = useState([]);
 
+  useEffect(() => {
+    fetchProducts()
+      .then(products => setData(products))
+      .catch(err => console.log(err));
+  }, []);
 
-    return (
-        <>Dashboard</>
-    )
+  return (
+    <>
+      <h1>Dashboard</h1>
+      <ul>
+        {data.map(product => (
+          <li key={product.id}>{product.name}</li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
-export default Dashboard
+export default Dashboard;
