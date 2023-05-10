ddd74ccfa2020805b98d3b97a739fa9312bf7272
vincentkho67 <122125309+vincentkho67@users.noreply.github.com>
2023-05-10 23:39:06 +0700

    Feature/cards (#4)
    * Simple Cards
    
    * feat: :sparkles: simple card
    
    ---------
    
    Co-authored-by: Yulius Pratama <yuliuspratama9e@gmail.com>

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,8 +1,157 @@
-const Cards = () => {
+import { Card,Text,Image,Stack,Heading,Divider,Button,ButtonGroup, CardHeader, CardBody, CardFooter } from '@chakra-ui/react'
+
+function Product({ product}) {
+    const {name,price,weight,size, description,SKU,image} = product;
+    return(
+        <>
+<Card maxW='sm'>
+<Heading>Product</Heading>
+  <CardBody>
+    <Image
+      src={image}
+      alt={name}
+      borderRadius='lg'
+    />
+    <Stack mt='6' spacing='3'>
+      <Heading size='md'>{name}</Heading>
+      <Text>
+        {description}
+      </Text>
+      <Text>
+        Berat : {weight}
+      </Text>
+      <Text>
+        Ukuran : {size}
+      </Text>
+      <Text>
+        SKU : {SKU}
+      </Text>
+      <Text color='blue.600' fontSize='2xl'>
+        Harga : {price}
+      </Text>
+    </Stack>
+  </CardBody>
+  <Divider />
+  <CardFooter>
+    <ButtonGroup spacing='2'>
+      <Button variant='solid' colorScheme='blue'>
+        Sunting
+      </Button>
+      <Button variant='ghost' colorScheme='blue'>
+        Add to Inv
+      </Button>
+    </ButtonGroup>
+  </CardFooter>
+</Card>
+</>
+    )
+  }
+
+function Warehouse({ warehouse}) {
+    const {name,city,address,image} = warehouse;
+    return(
+        <>
+<Card maxW='sm'>
+<Heading>Warehouse</Heading>
+  <CardBody>
+    <Image
+      src={image}
+      alt={name}
+      borderRadius='lg'
+    />
+    <Stack mt='6' spacing='3'>
+      <Heading size='md'>{name}</Heading>
+      <Text>
+        alamat : {address}
+      </Text>
+      <Text>
+        Kota : {city}
+      </Text>
+    </Stack>
+  </CardBody>
+  <Divider />
+  <CardFooter>
+    <ButtonGroup spacing='2'>
+      <Button variant='solid' colorScheme='blue'>
+        Sunting
+      </Button>
+      <Button variant='ghost' colorScheme='blue'>
+        Add to Inv
+      </Button>
+    </ButtonGroup>
+  </CardFooter>
+</Card>
+</>
+    )
+  }
+
+function Categori({ categori}) {
+    const {name,description,image} = categori;
+    return(
+        <>
+<Card maxW='sm'>
+<Heading>Categori</Heading>
+  <CardBody>
+    <Image
+      src={image}
+      alt={name}
+      borderRadius='lg'
+    />
+    <Stack mt='6' spacing='3'>
+      <Heading size='md'>{name}</Heading>
+      <Text>
+        {description}
+      </Text>
+    </Stack>
+  </CardBody>
+  <Divider />
+  <CardFooter>
+    <ButtonGroup spacing='2'>
+      <Button variant='solid' colorScheme='blue'>
+        Sunting
+      </Button>
+      <Button variant='ghost' colorScheme='blue'>
+        Add to Inv
+      </Button>
+    </ButtonGroup>
+  </CardFooter>
+</Card>
+</>
+    )
+  }
+
+const Cards = ({props,type}) => {
+    switch (type) {
+        case "product":
+            return(
+                <>
+                <Product product={props} />
+                </>
+            )
+            break;
+        case "warehouse":
+            return(
+                <>
+                <Warehouse warehouse={props}/>
+                </>
+            )
+            break;
+        case "categori":
+            return(
+                <>
+                <Categori categori={props}/>
+                </>
+            )
+            break;
+    
+        default:
+            break;
+    }
 
 
-    return( 
-        <></>
+return( 
+<>
+</>
     )
 };
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,43 @@
 import LoginPage from '../components/Login'
 import Landing from '../components/Landing'
-import { Container, HStack } from '@chakra-ui/react'
+import { Container, SimpleGrid } from '@chakra-ui/react'
+import Cards from '@/components/Cards';
 
 const Home = () => {
+  const type = "warehouse"
+   
+  const product =         
+  {
+      name: "Keripik Tela",
+      price: 100,
+      weight: "1 Kg",
+      size: "20 Cm",
+      description: "Keripik tela curah",
+      SKU: "KTC1",
+      image : "https://source.unsplash.com/random/900x700/?vegetable",
+    }
+
+  const warehouse =         
+  {
+      name: "Gudang Barang Depok 1",
+      city: "Depok",
+      address: "Jl Penghasahan No 15",
+      image : "https://source.unsplash.com/random/900x700/?Build",
+  }
+  const categori =         
+  {
+      name: "Grocery",
+      description: " Kebutuhan Sehari Hari ",
+      image : "https://source.unsplash.com/random/900x700/?Items",
+  }
 
   return(
     <Container>
-      <HStack>    
-        <Landing/>
-        <LoginPage/>
-      </HStack>  
+      <SimpleGrid spacing={8} templateColumns='repeat(auto-fill, minmax(200px, 1fr))'>   
+        <Cards props={warehouse} type={"warehouse"}/>
+        <Cards props={categori} type={"categori"}/>
+        <Cards props={product} type={"product"}/>
+      </SimpleGrid>
     </Container>
   )
 };
