6ceee2bdbc7b92abc0af90b53dbd8d057491fb66
Yulius Pratama <yuliuspratama9e@gmail.com>
2023-05-10 10:52:14 +0700

    Simple Cards

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,8 +1,157 @@
-const Cards = () => {
+import { Card,Text,Image,Stack,Heading,Divider,Button,ButtonGroup, CardHeader, CardBody, CardFooter } from '@chakra-ui/react'
+
+function Product({ product}) {
+    const {name,price,weight,size, description,SKU} = product;
+    return(
+        <>
+<Card maxW='sm'>
+<Heading>Product</Heading>
+  <CardBody>
+    <Image
+      src='https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
+      alt='Green double couch with wooden legs'
+      borderRadius='lg'
+    />
+    <Stack mt='6' spacing='3'>
+      <Heading size='md'>{name}</Heading>
+      <Text>
+        {description}
+      </Text>
+      <Text>
+        Berat : {weight}
+      </Text>
+      <Text>
+        Ukuran : {size}
+      </Text>
+      <Text>
+        SKU : {SKU}
+      </Text>
+      <Text color='blue.600' fontSize='2xl'>
+        Harga : {price}
+      </Text>
+    </Stack>
+  </CardBody>
+  <Divider />
+  <CardFooter>
+    <ButtonGroup spacing='2'>
+      <Button variant='solid' colorScheme='blue'>
+        Sunting
+      </Button>
+      <Button variant='ghost' colorScheme='blue'>
+        Add to Inv
+      </Button>
+    </ButtonGroup>
+  </CardFooter>
+</Card>
+</>
+    )
+  }
+
+function Warehouse({ warehouse}) {
+    const {name,city,address} = warehouse;
+    return(
+        <>
+<Card maxW='sm'>
+<Heading>Warehouse</Heading>
+  <CardBody>
+    <Image
+      src='https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
+      alt='Green double couch with wooden legs'
+      borderRadius='lg'
+    />
+    <Stack mt='6' spacing='3'>
+      <Heading size='md'>{name}</Heading>
+      <Text>
+        alamat : {address}
+      </Text>
+      <Text>
+        Kota : {city}
+      </Text>
+    </Stack>
+  </CardBody>
+  <Divider />
+  <CardFooter>
+    <ButtonGroup spacing='2'>
+      <Button variant='solid' colorScheme='blue'>
+        Sunting
+      </Button>
+      <Button variant='ghost' colorScheme='blue'>
+        Add to Inv
+      </Button>
+    </ButtonGroup>
+  </CardFooter>
+</Card>
+</>
+    )
+  }
+
+function Categori({ categori}) {
+    const {name,description} = categori;
+    return(
+        <>
+<Card maxW='sm'>
+<Heading>Warehouse</Heading>
+  <CardBody>
+    <Image
+      src='https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
+      alt='Green double couch with wooden legs'
+      borderRadius='lg'
+    />
+    <Stack mt='6' spacing='3'>
+      <Heading size='md'>{name}</Heading>
+      <Text>
+        {description}
+      </Text>
+    </Stack>
+  </CardBody>
+  <Divider />
+  <CardFooter>
+    <ButtonGroup spacing='2'>
+      <Button variant='solid' colorScheme='blue'>
+        Sunting
+      </Button>
+      <Button variant='ghost' colorScheme='blue'>
+        Add to Inv
+      </Button>
+    </ButtonGroup>
+  </CardFooter>
+</Card>
+</>
+    )
+  }
+
+const Cards = ({props,type}) => {
+    switch (type) {
+        case "product":
+            return(
+                <>
+                <Product product={props} />
+                </>
+            )
+            break;
+        case "warehouse":
+            return(
+                <>
+                <Warehouse warehouse={props}/>
+                </>
+            )
+            break;
+        case "categori":
+            return(
+                <>
+                <Categori categori={props}/>
+                </>
+            )
+            break;
+    
+        default:
+            break;
+    }
 
 
-    return( 
-        <></>
+return( 
+<>
+</>
     )
 };
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,37 @@
 import LoginPage from '../components/Login'
 import Landing from '../components/Landing'
 import { Container, HStack } from '@chakra-ui/react'
+import Cards from '@/components/Cards';
 
 const Home = () => {
+  const type = "warehouse"
+   
+  const product =         
+  {
+      name: "Keripik Tela",
+      price: 100,
+      weight: "1 Kg",
+      size: "20 Cm",
+      description: "Keripik tela curah",
+      SKU: "KTC1",
+    }
+
+  const warehouse =         
+  {
+      name: "Gudang Barang Depok 1",
+      city: "Depok",
+      address: "Jl Penghasahan No 15",
+  }
+  const categori =         
+  {
+      name: "Grocery",
+      description: " Kebutuhan Sehari Hari ",
+  }
 
   return(
     <Container>
       <HStack>    
-        <Landing/>
-        <LoginPage/>
+        <Cards props={warehouse} type={type}/>
       </HStack>  
     </Container>
   )
