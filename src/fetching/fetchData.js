a42b5e15641ad19decf55f622ca69054f8054ff2
vincentkho67 <122125309+vincentkho67@users.noreply.github.com>
2023-05-10 23:27:49 +0700

    Get all fetch (#3)
    * fetching of all data on the dashboard completed
    
    * proper identation
    
    * explanation on fetchData.js
    
    * starter
    
    * almost done fetching on dashboard

--- src/fetching/fetchData.js
+++ src/fetching/fetchData.js
@@ -12,6 +12,56 @@ const fetchData = async (url) => {
     const data = await response.json();
     return data;
 }
+/*
+The current json res from backend would output 3 different objects
+products = [{}]
+data = [{}]
+array of objects directly [{}]
+
+all of the payloads and req.body
+=======================================================
+
+products = products = [{}]
+orders = data = [{},{}] id, user_id customer_id warehouse_id name total_price -> include Customer: {} include Warehouse: {}
+orderproducts = [{},{}] product_id order_id price quantity
+warehousestocks = [{},{}] product_id, warehouse_id, quantity
+expenses = [{}] id user_id expense detail
+revenues = [{}] id user_id revenue detail
+vendors = [{}] id user_id name country
+warehouses = [{}] id user_id name city address
+categories = [{}] id name description
+productvendors = [{}] product_id vendor_id
+*/
+
+export const fetchStocks= async () => {
+    const data = await fetchData('warehousestocks');
+    return data;
+};
+
+export const fetchOrderDetails = async () => {
+    const data = await fetchData('orderproducts');
+    return data;
+};
+
+export const fetchVendors = async () => {
+    const data = await fetchData('vendors');
+    return data;
+};
+
+export const fetchWarehouses = async () => {
+    const data = await fetchData('warehouses');
+    return data;
+};
+
+export const fetchRevenues = async () => {
+    const data = await fetchData('revenues');
+    return data;
+};
+
+export const fetchExpenses = async () => {
+    const data = await fetchData('expenses');
+    return data;
+};
 
 export const fetchProducts = async () => {
     const data = await fetchData('products');
@@ -25,5 +75,10 @@ export const fetchOrders = async () => {
 
 export const fetchCustomers = async () => {
     const data = await fetchData('customers');
+    return data;
+};
+
+export const fetchCategories = async () => {
+    const data = await fetchData('categories');
     return data;
 };
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,38 +1,130 @@
 import { useState, useEffect } from 'react';
-import { fetchProducts, fetchOrders, fetchCustomers } from '@/fetching/fetchData';
+import { fetchProducts, fetchOrders, fetchCustomers, fetchExpenses, fetchRevenues, fetchOrderDetails, fetchStocks, fetchVendors, fetchWarehouses, fetchCategories } from '@/fetching/fetchData';
+import { Table, Thead, Tbody, Tr, Th, Td, Select, Container } from "@chakra-ui/react";
 
 const Dashboard = () => {
-  const [data, setData] = useState({ products: [], orders: [], customers: [] });
+  const [data, setData] = useState(
+    { products: [], 
+      orders: [], 
+      customers: [],
+      expenses: [],
+      revenues: [],
+      orderDetails: [],
+      stocks: [],
+      vendors: [],
+      warehouses: [],
+      categories:[]
+    });
 
   useEffect(() => {
-    Promise.all([fetchProducts(), fetchOrders(), fetchCustomers()])
-      .then(([productsData, ordersData, customersData]) => {
-        setData({ products: productsData, orders: ordersData, customers: customersData });
-        console.log(customersData);
-      })
+    Promise.all(
+      [
+        fetchProducts(), fetchOrders(), fetchCustomers(),
+        fetchCategories(),fetchExpenses(),fetchRevenues(),
+        fetchOrderDetails(),fetchStocks(),fetchVendors(),
+        fetchWarehouses()
+      ]
+      )
+      .then((
+        [
+          productsData, ordersData, customersData, expensesData, revenuesData,
+          orderDetailsData, stocksData, vendorsData, warehousesData, categoriesData
+        ]
+      ) => {
+        setData({
+          products: productsData, 
+          orders: ordersData, 
+          customers: customersData,
+          expenses: expensesData,
+          revenues: revenuesData,
+          orderDetails: orderDetailsData,
+          stocks: stocksData,
+          vendors: vendorsData,
+          warehouses: warehousesData,
+          categories: categoriesData 
+        });
+      })      
       .catch(err => console.log(err));
   }, []);
   
+  const products = data.products;
+
+  function renderProduct(products) {
+    return products.map((product) => {
+      const warehousesForProduct = product.Warehouses.map((warehouse) => ({
+        id: product.id,
+        name: warehouse.name,
+        WarehouseStock: warehouse.WarehouseStock
+      }));
+
+      const totalQuantity = warehousesForProduct.reduce((acc, warehouse) => acc + warehouse.WarehouseStock.quantity, 0);
+
+      const vendorsForProduct = product.Vendors.map((vendor) => ({
+        id: product.id,
+        name: vendor.name
+      }));
+
+      const warehouseSelect =
+        warehousesForProduct.length > 1 ? (
+          <Select>
+            {warehousesForProduct.map((warehouse) => (
+              <option key={warehouse.name}>
+                {warehouse.name} Q({warehouse.WarehouseStock.quantity})
+              </option>
+            ))}
+          </Select>
+        ) : (
+          <span>{warehousesForProduct[0]?.name} ({warehousesForProduct[0]?.WarehouseStock.quantity})</span>
+        );
+      
+      const vendorSelect =
+        vendorsForProduct.length > 1 ? (
+          <Select>
+            {vendorsForProduct.map((vendor) => (
+              <option key={vendor.name}>
+                {vendor.name}
+              </option>
+            ))}
+          </Select>
+        ) : (
+          <span>{vendorsForProduct[0]?.name}</span>
+        );
+
+        return (
+          <Tr key={product.id}>
+            <Td>{product.name}</Td>
+            <Td>
+              {product.Categories.map((category) => (
+                <span key={category.id}>{category.name}</span>
+              ))}
+            </Td>
+            <Td>{warehouseSelect}</Td>
+            <Td>{vendorSelect}</Td>
+            <Td>{totalQuantity}</Td>
+          </Tr>
+        );
+    });
+  }
+
+  const tableBody = renderProduct(products);
+
 
   return (
-    <>
-      <h1>Dashboard</h1>
-      <ul>
-        {data.products.map((product) => (
-          <li key={product.id}>{product.name}</li>
-        ))}
-        {data.orders.map((order) => (
-            <li key={order.id}>{order.name} price: {order.total_price}</li>
-        ))}
-        {data.customers.map((customer) => (
-            <li key={customer.id}>{customer.first_name}</li>
-        ))}
-
-      </ul>
-    </>
+    <Container maxW="container.x1">
+      <Table>
+        <Thead>
+          <Tr>
+            <Th>Products List</Th>
+            <Th>Category</Th>
+            <Th>Warehouse</Th>
+            <Th>Vendor</Th>
+            <Th>Quantity</Th>
+          </Tr>
+        </Thead>
+        <Tbody>{tableBody}</Tbody>
+      </Table>
+    </Container>
   );
-  
-  
-};
+}
 
 export default Dashboard;
