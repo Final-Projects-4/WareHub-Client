8588e8397a5cc520b1948a3e4bc2567f88aa064b
Vincent Kho <vincentkho67@gmail.com>
2023-05-14 07:11:53 +0700

    coba merge landing & login

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -5,66 +5,67 @@ import { useState, useEffect } from "react";
 import { ProfitLoss } from "@/components/dataComponents/profitLoss";
 
 const Dashboard = () => {
-  const [chartsSize, setChartsSize] = useState("100%");
+  // const [chartsSize, setChartsSize] = useState("100%");
 
-  const handleResize = () => {
-    if (window.innerWidth > 768) {
-      setChartsSize("100%");
-    } else {
-      setChartsSize("50%");
-    }
-  };
+  // const handleResize = () => {
+  //   if (window.innerWidth > 768) {
+  //     setChartsSize("100%");
+  //   } else {
+  //     setChartsSize("50%");
+  //   }
+  // };
   
-  useEffect(() => {
-    handleResize(); // set the initial size when the component mounts
-    window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
+  // useEffect(() => {
+  //   handleResize(); // set the initial size when the component mounts
+  //   window.addEventListener("resize", handleResize);
+  //   return () => {
+  //     window.removeEventListener("resize", handleResize);
+  //   };
+  // }, []);
   
-  const products = data.products;
-  const totalWarehouses = [];
-  let totalQuantity = 0;
+  // const products = data.products;
+  // const totalWarehouses = [];
+  // let totalQuantity = 0;
 
-  if (products.length > 0) {
-    products.forEach((product) => {
-      product.Warehouses.forEach((warehouse) => {
-        totalWarehouses.push(warehouse.name)
-      });
-    });
-  }
+  // if (products.length > 0) {
+  //   products.forEach((product) => {
+  //     product.Warehouses.forEach((warehouse) => {
+  //       totalWarehouses.push(warehouse.name)
+  //     });
+  //   });
+  // }
   
   
   
 
 
-  function renderProduct(products) {
+  // function renderProduct(products) {
     
 
     
 
-    return products.map((product) => (
-          <Tr key={product.id}>
-            <Td>{product.name}</Td>
-            <Td>{product.Categories.map((category) => (<span key={category.id}>{category.name}</span>))}</Td>
-            <Td></Td>
-          </Tr>
-    ));
+  //   return products.map((product) => (
+  //         <Tr key={product.id}>
+  //           <Td>{product.name}</Td>
+  //           <Td>{product.Categories.map((category) => (<span key={category.id}>{category.name}</span>))}</Td>
+  //           <Td></Td>
+  //         </Tr>
+  //   ));
     
     
 
-  }
+  // }
 
 
   return (
     <>
-      <Flex>
+      {/* <Flex>
         <Flex flex="1" w="100%" p="3%" flexDir="column" overflow="auto" minH="100vh">
           <Charts w={chartsSize} />
           <ProfitLoss/>
         </Flex>
       </Flex>
+    </> */}
     </>
   );
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,20 @@
 import LoginPage from "../components/Login";
 import Landing from "../components/Landing";
-import { Container, SimpleGrid, Box } from "@chakra-ui/react";
+import { Container, Box, Flex } from "@chakra-ui/react";
 
 
 const Home = () => {
   return (
-    <Container>
-      <LoginPage position="absolute" top={0} left={0} zIndex={1} />
-        <Box position="relative" zIndex={0}>
-          <SimpleGrid spacing={8} templateColumns="repeat(auto-fill, minmax(200px, 1fr))">
-          </SimpleGrid>
-        </Box>
-    </Container>
+    <Flex>
+        <Landing zIndex={-1}/>
+        <LoginPage position="absolute" top={0} left={0} zIndex={1} />
+    </Flex>
+        
+     
   );
 };
+
+
+
 
 export default Home;
